339718025c8d768413b834c9383e073365172f07
Sarath Kumar M <goldenvalley4ever@gmail.com>
2018-08-16 15:39:12 +0530

    annotations

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -3,10 +3,21 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 class ArticleController {
 
+        /**
+         * @Route("/")
+         */
         public function homepage() {
-            return new Response('OMG! My first page already! WOOO!');
+            return new Response('OMG! My first page already! WOOO! Test');
+        }
+
+        /**
+         * @Route("/news/{slug}")
+         */
+        public function show($slug) {
+            return new Response(sprintf('Future space for showing: %s', $slug));
         }
 }
