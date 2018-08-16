716c3db0de6aea26275df21b88900191803610c3
Sarath Kumar M <goldenvalley4ever@gmail.com>
2018-08-16 19:34:14 +0530

    json

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -28,7 +28,18 @@ class ArticleController extends AbstractController{
 
             return $this->render('article/show.html.twig', [
                 'title' => ucwords(str_replace('-', ' ', $slug)),
+                'slug' => $slug,
                 'comments' => $comments
             ]);
+        }
+
+        /**
+         * @Route("/news/{slug}/heart", name="article_toggle_heart", methods={"POST"})
+         */
+        public function toggleArticleHeart($slug) {
+
+            // TODO - actually heart/unheart the article!
+
+            return $this->json(['hearts' => rand(5, 100)]);
         }
 }
