9147d997e32eda44e386bf1041239712680544cc
Sarath Kumar M <goldenvalley4ever@gmail.com>
2018-08-21 06:18:53 +0530

    query fetch

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
 use App\Service\SlackClient;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Psr\Log\LoggerInterface;
@@ -20,7 +22,7 @@ class ArticleController extends AbstractController{
         /**
          * @Route("/news/{slug}", name="app_show")
          */
-        public function show($slug, MarkdownHelper $markdownHelper, SlackClient $slack) {
+        public function show($slug, MarkdownHelper $markdownHelper, SlackClient $slack, EntityManagerInterface $em) {
 
             if ($slug == 'khan') {
                 $slack->sendMessage('Khan', 'Hi friend');
@@ -32,41 +34,19 @@ class ArticleController extends AbstractController{
                 'I like bacon too! Buy some from my site! bakinsomebacon.com',
             ];
 
-            $articleContent =<<<EOF
-            Spicy **jalapeno bacon** ipsum dolor amet veniam shank in dolore. Ham hock nisi landjaeger cow,
-            lorem proident beef ribs aute enim veniam ut cillum pork chuck picanha. Dolore reprehenderit
-            labore minim pork belly spare ribs cupim short loin in. Elit exercitation eiusmod dolore cow
-            turkey shank eu pork belly meatball non cupim.</p>
+            $repository = $em->getRepository(Article::class);
 
-        Laboris beef ribs fatback fugiat eiusmod jowl kielbasa alcatra dolore velit ea ball tip. Pariatur
-            laboris sunt venison, et laborum dolore minim non meatball. Shankle eu flank aliqua shoulder,
-            capicola biltong frankfurter boudin cupim officia. Exercitation fugiat consectetur ham. Adipisicing
-            picanha shank et filet mignon pork belly ut ullamco. Irure velit turducken ground round doner incididunt
-            occaecat lorem meatball prosciutto quis strip steak.</p>
+            /** @var Article $article */
+            $article = $repository->findOneBy(['slug' => $slug]);
 
-        Meatball adipisicing ribeye bacon strip steak eu. Consectetur ham hock pork hamburger enim strip steak
-            mollit quis officia meatloaf tri-tip swine. Cow ut reprehenderit, buffalo incididunt in filet mignon
-            strip steak pork belly aliquip capicola officia. Labore deserunt esse chicken lorem shoulder tail consectetur
-            cow est ribeye adipisicing. Pig hamburger pork belly enim. Do porchetta minim capicola irure pancetta chuck
-            fugiat.</p>
+            if (!$article) {
+                throw $this->createNotFoundException(sprintf('Article with "%s" not found', $slug));
+            }
 
-        Sausage tenderloin officia jerky nostrud. Laborum elit pastrami non, pig kevin buffalo minim ex quis. Pork belly
-            pork chop officia anim. Irure tempor leberkas kevin adipisicing cupidatat qui buffalo ham aliqua pork belly
-            exercitation eiusmod. Exercitation incididunt rump laborum, t-bone short ribs buffalo ut shankle pork chop
-            bresaola shoulder burgdoggen fugiat. Adipisicing nostrud chicken consequat beef ribs, quis filet mignon do.
-            Prosciutto capicola mollit shankle aliquip do dolore hamburger brisket turducken eu.</p>
 
-        Do mollit deserunt prosciutto laborum. Duis sint tongue quis nisi. Capicola qui beef ribs dolore pariatur.
-            Minim strip steak fugiat nisi est, meatloaf pig aute. Swine rump turducken nulla sausage. Reprehenderit pork
-            belly tongue alcatra, shoulder excepteur in beef bresaola duis ham bacon eiusmod. Doner drumstick short loin,
-            adipisicing cow cillum tenderloin.
-EOF;
-
-            $articleContent = $markdownHelper->parse($articleContent);
+            //$articleContent = $markdownHelper->parse($articleContent);
             return $this->render('article/show.html.twig', [
-                'title' => ucwords(str_replace('-', ' ', $slug)),
-                'slug' => $slug,
-                'articleContent' => $articleContent,
+                'article' => $article,
                 'comments' => $comments
             ]);
         }
